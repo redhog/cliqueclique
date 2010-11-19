069091f55fb02a43548295d89d027c08fa907515
Egil Moeller <egil.moller@freecode.no>
2010-11-19 18:07:53 +0100

    Slight style fixes

--- cliqueclique_subscription/models.py
+++ cliqueclique_subscription/models.py
@@ -15,23 +15,27 @@ class BaseDocumentSubscription(django.db.models.Model):
 
     node = django.db.models.ForeignKey(cliqueclique_node.models.LocalNode, related_name="subscriptions")
 
-    center_node_is_subscribed = django.db.models.BooleanField()
-    center_node_id = django.db.models.CharField(max_length=settings.CLIQUECLIQUE_HASH_LENGTH)
-    center_distance = django.db.models.IntegerField()
+    center_node_is_subscribed = django.db.models.BooleanField(default=False)
+    center_node_id = django.db.models.CharField(max_length=settings.CLIQUECLIQUE_HASH_LENGTH, null=True, blank=True)
+    center_distance = django.db.models.IntegerField(default = 0)
 
 class DocumentSubscription(BaseDocumentSubscription):
     document = django.db.models.ForeignKey(cliqueclique_document.models.Document, related_name="subscriptions")
 
-    # Note, this might be NULL if no document with self.document.{up,down}_document_id
+    # Note, this might be empty if no document with self.document.{up,down}_document_id
     # exists at this node yet
-    parent = django.db.models.ForeignKey("DocumentSubscription", related_name="children", null=True, blank=True)
+    parents = django.db.models.ManyToManyField("DocumentSubscription", related_name="children", null=True, blank=True)
 
     # Did _we_ subscribe to this?
-    local_is_subscribed = django.db.models.BooleanField()
-    subscribers = django.db.models.SmallIntegerField()
+    local_is_subscribed = django.db.models.BooleanField(default = False)
+    subscribers = django.db.models.SmallIntegerField(default = 0) # Don't change this one manually
 
     @property
-    def is_subscribed(self): return self.subscribers > 0
+    def is_subscribed(self): return self.subscribers > 0 or self.local_is_subscribed
+
+    @classmethod
+    def subscription_for_document(cls, node, document):
+        return document.subscriptions.get(node=node)
 
     def peer_subscription(self, peer):
         try:
@@ -39,9 +43,34 @@ class DocumentSubscription(BaseDocumentSubscription):
         except:
             return None
 
+    def update_child_subscriptions(self):
+        if self.document.parent_document_id is not None:
+            parent_document = cliqueclique_document.models.Document.objects.get(document_id=self.document.parent_document_id)
+            parent_subscription = self.subscription_for_document(self.node, parent_document) 
+            if parent_subscription not in self.parents.all():
+                self.parents.add(parent_subscription)
+                self.save()
+
+        if self.document.child_document_id is not None:
+            child_document = cliqueclique_document.models.Document.objects.get(document_id=self.document.child_document_id)
+            child_subscription = self.subscription_for_document(self.node, child_document) 
+            if child_subscription not in self.children.all():
+                self.children.add(child_subscription)
+                self.save()
+
+    @classmethod
+    def pre_save(cls, sender, instance, **kwargs):
+        self = instance
+        if self.center_node_id is None:
+            self.center_node_is_subscribed = self.is_subscribed
+            self.center_node_id = self.node.node_id
+            self.center_distance = 0
+
     @classmethod
     def post_save(cls, sender, instance, **kwargs):
-        for peer_subscription in instance.peer_subscriptions:
+        self = instance
+        self.update_child_subscriptions()
+        for peer_subscription in self.peer_subscriptions.all():
             peer_subscription.update_child_subscriptions()
             peer_subscription.set_dirty()
 
@@ -52,15 +81,15 @@ class PeerDocumentSubscription(DocumentSubscription):
     local_subscription = django.db.models.ForeignKey(DocumentSubscription, related_name="peer_subscriptions")
     peer = django.db.models.ForeignKey(cliqueclique_node.models.Peer, related_name="subscriptions")
 
-    is_dirty = django.db.models.BooleanField()
+    is_dirty = django.db.models.BooleanField(default = True)
 
-    is_subscribed = django.db.models.BooleanField()
+    is_subscribed = django.db.models.BooleanField(default = False)
  
-    peer_old_is_subscribed = django.db.models.BooleanField() # Don't modify this by hand, it's just for internal use by the pre_save method
-    peer_is_subscribed = django.db.models.BooleanField()
-    peer_center_node_is_subscribed = django.db.models.BooleanField()
-    peer_center_node_id = django.db.models.CharField(max_length=settings.CLIQUECLIQUE_HASH_LENGTH)
-    peer_center_distance = django.db.models.IntegerField()
+    peer_old_is_subscribed = django.db.models.BooleanField(default = False) # Don't modify this by hand, it's just for internal use by the pre_save method
+    peer_is_subscribed = django.db.models.BooleanField(default = False)
+    peer_center_node_is_subscribed = django.db.models.BooleanField(default = False)
+    peer_center_node_id = django.db.models.CharField(max_length=settings.CLIQUECLIQUE_HASH_LENGTH, null=True, blank=True)
+    peer_center_distance = django.db.models.IntegerField(default = 0)
 
     @property
     def subscribers(self): return [0, 1][self.is_subscribed]
@@ -102,23 +131,24 @@ class PeerDocumentSubscription(DocumentSubscription):
 
     @classmethod
     def pre_save(cls, sender, instance, **kwargs):
-        if not instance.is_dirty:
-            if instance.is_upstream(1):
-                local = instance.local_subscription
-                peer = curryprefix(instance, "peer_")
+        self = instance
+        if not self.is_dirty:
+            if self.is_upstream(1):
+                local = self.local_subscription
+                peer = curryprefix(self, "peer_")
 
                 local.center_node_is_subscribed = peer.center_node_is_subscribed
                 local.center_node_id = peer.center_node_id
                 local.center_distance = peer.center_distance + 1
                 local.save()
 
-            elif instance.is_downstream(1):
+            elif self.is_downstream(1):
 
-                local = instance.local_subscription
-                peer = curryprefix(instance, "peer_")
+                local = self.local_subscription
+                peer = curryprefix(self, "peer_")
 
                 if peer.subscribers != peer.old_subscribers:
                     local.subscribers += peer.subscribers - peer.old_subscribers
                     peer.old_is_subscribed = peer.is_subscribed
                     local.save()
-                    instance.save()
+                    self.save()
--- cliqueclique_subscription/tests.py
+++ cliqueclique_subscription/tests.py
@@ -1,23 +1,37 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+import django.test
+import cliqueclique_node.models
+import cliqueclique_document.models
+import cliqueclique_subscription.models
 
-Replace these with more appropriate tests for your application.
-"""
+def save(obj):
+    obj.save()
+    return obj
 
-from django.test import TestCase
+class SimpleTest(django.test.TestCase):
+    def setUp(self):
+        self.local_node = save(cliqueclique_node.models.LocalNode(node_id="local", public_key="X", address="localhost", private_key="X"))
+        self.peers = [save(cliqueclique_node.models.Peer(node = self.local_node, node_id="peer%s" % n, public_key="X", address="localhost")) for n in xrange(0, 2)]
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+#        self.documents = [save(cliqueclique_document.models.Document(document_id="document%s" % n, content="content%s" % n)) for n in xrange(0, 2)]
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def test_child_links(self):
+        n = "test_child_links"
+        root_doc = save(cliqueclique_document.models.Document(document_id=n+"root", content="root content"))
+        child_doc = save(cliqueclique_document.models.Document(document_id=n+"child", content="child content", parent_document_id=n+"root"))
+        parent_doc = save(cliqueclique_document.models.Document(document_id=n+"parent", content="parent content", child_document_id=n+"root"))
 
->>> 1 + 1 == 2
-True
-"""}
+        root_sub = save(cliqueclique_subscription.models.DocumentSubscription(
+                node = self.local_node,
+                document = root_doc))
 
+        child_sub = save(cliqueclique_subscription.models.DocumentSubscription(
+                node = self.local_node,
+                document = child_doc))
+
+        parent_sub = save(cliqueclique_subscription.models.DocumentSubscription(
+                node = self.local_node,
+                document = parent_doc))
+
+        self.assertTrue(len(parent_sub.children.all()) == 1)
+        self.assertTrue(len(root_sub.children.all()) == 1)
+        self.assertEqual(root_sub.children.all()[0].document.content, "child content")
