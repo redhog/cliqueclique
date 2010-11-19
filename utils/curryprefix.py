27c283dd1a88e3bf90a1045fb7c2ff2a94775381
Egil Moeller <egil.moller@freecode.no>
2010-11-19 20:25:47 +0100

    Support for setting attributes using curryprefix

--- utils/curryprefix.py
+++ utils/curryprefix.py
@@ -1,7 +1,10 @@
 class curryprefix(object):
     def __init__(self, obj, prefix):
-        self.obj = obj
-        self.prefix = prefix
+        self.__dict__['obj'] = obj
+        self.__dict__['prefix'] = prefix
 
     def __getattr__(self, name):
         return getattr(self.obj, self.prefix + name)
+
+    def __setattr__(self, name, value):
+        return setattr(self.obj, self.prefix + name, value)
