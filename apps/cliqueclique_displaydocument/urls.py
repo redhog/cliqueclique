fbbddebc852a8e21625ff65288fd5abe852ebc24
Egil Moeller <egil.moller@freecode.no>
2010-11-30 22:19:51 +0100

    Added the ability to download a document

--- apps/cliqueclique_displaydocument/urls.py
+++ apps/cliqueclique_displaydocument/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    (r'^(?P<document_id>[^/]*)/download', "cliqueclique_displaydocument.views.download"),
     (r'^(?P<document_id>[^/]*)', "cliqueclique_displaydocument.views.display"),
 )
--- apps/cliqueclique_displaydocument/views.py
+++ apps/cliqueclique_displaydocument/views.py
@@ -4,6 +4,7 @@ import django.core.urlresolvers
 import cliqueclique_document.models
 import cliqueclique_subscription.models
 import email.mime.text
+import django.http
 
 def display(request, document_id):
     if 'post' in request.POST:
@@ -25,3 +26,9 @@ def display(request, document_id):
     info['body'] = info['document_subscription'].document.as_mime.get_payload()
 
     return django.shortcuts.render_to_response('cliqueclique_displaydocument/display.html', info, context_instance=django.template.RequestContext(request))
+
+def download(request, document_id):
+    doc = cliqueclique_subscription.models.DocumentSubscription.objects.get(
+        node = request.user.node,
+        document__document_id=document_id)
+    return django.http.HttpResponse(doc.send(True).as_string(), mimetype="text/plain")
