41d70975dbc1b6599afa0b40797f1c8a7896228e
Egil Moeller <egil.moller@freecode.no>
2010-12-02 19:36:20 +0100

    Better grouping of subscription attributes in admin

--- apps/cliqueclique_subscription/admin.py
+++ apps/cliqueclique_subscription/admin.py
@@ -1,5 +1,33 @@
 import django.contrib.admin
 import cliqueclique_subscription.models
 
-django.contrib.admin.site.register(cliqueclique_subscription.models.PeerDocumentSubscription)
 django.contrib.admin.site.register(cliqueclique_subscription.models.DocumentSubscription)
+
+class PeerDocumentSubscriptionAdmin(django.contrib.admin.ModelAdmin):
+    fieldsets = [("The subscription of the peer",
+                  {'fields': ('peer_old_is_subscribed',
+                              'peer_is_subscribed',
+                              'peer_center_node_is_subscribed',
+                              'peer_center_node_id',
+                              'peer_center_distance',
+                              'has_copy')}),
+                 ("Peers' knowledge about us",
+                  {'fields': ('is_subscribed',
+                              'center_node_is_subscribed',
+                              'center_node_id',
+                              'center_distance')}),
+                 
+
+                 ('Metadata',
+                  {'fields': ('peer',
+                              'local_subscription')}),
+                 ]
+
+    list_display_links = list_display = (
+        'local_subscription',
+        'peer',
+        'is_dirty')
+    search_fields = ()
+
+
+django.contrib.admin.site.register(cliqueclique_subscription.models.PeerDocumentSubscription, PeerDocumentSubscriptionAdmin)
