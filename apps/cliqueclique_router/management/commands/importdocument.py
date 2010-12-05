8630e76e8c642b17d28f60734f952646add6959b
Egil Moeller <egil.moller@freecode.no>
2010-12-05 17:28:53 +0100

    Ability to bookmark documents when importing/posting using the command line utils

--- apps/cliqueclique_router/management/commands/importdocument.py
+++ apps/cliqueclique_router/management/commands/importdocument.py
@@ -12,6 +12,19 @@ import utils.smime
 class Command(BaseCommand):
     args = ''
 
+    option_list = BaseCommand.option_list + (
+        make_option('--bookmark',
+                    action='store_true',
+                    default=False,
+                    dest='bookmark',
+                    help='Bookmark the document'),
+        make_option('--read',
+                    action='store_true',
+                    default=False,
+                    dest='read',
+                    help='Mark the document as read'),
+        )
+
     help = 'Import a document. Params: node_id filename'
 
     def handle(self, node_id, filename, *args, **options):
@@ -26,3 +39,12 @@ class Command(BaseCommand):
         print "Importing %s..." % (update_msg['document_id'],)
 
         node.receive(data)
+
+        sub = cliqueclique_subscription.models.DocumentSubscription.objects.get(
+            node = node,
+            document__document_id = update_msg['document_id'])
+
+        sub.bookmarked = options['bookmark']
+        sub.read = options['read']
+
+        sub.save()
--- apps/cliqueclique_router/management/commands/postdocument.py
+++ apps/cliqueclique_router/management/commands/postdocument.py
@@ -12,6 +12,19 @@ import utils.smime
 class Command(BaseCommand):
     args = ''
 
+    option_list = BaseCommand.option_list + (
+        make_option('--bookmark',
+                    action='store_true',
+                    default=False,
+                    dest='bookmark',
+                    help='Bookmark the document'),
+        make_option('--read',
+                    action='store_true',
+                    default=False,
+                    dest='read',
+                    help='Mark the document as read'),
+        )
+
     help = 'Post a new document. Params: node_id filename'
 
     def handle(self, node_id, filename, *args, **options):
@@ -21,7 +34,12 @@ class Command(BaseCommand):
 
         doc = cliqueclique_document.models.Document(content=data)
         doc.save()
-        sub = cliqueclique_subscription.models.DocumentSubscription(node = node, document = doc, local_is_subscribed=True)
+        sub = cliqueclique_subscription.models.DocumentSubscription(
+            node = node,
+            document = doc,
+            local_is_subscribed = True,
+            bookmarked = options['bookmark'],
+            read = options['read'])
         sub.save()
 
         print doc.document_id
