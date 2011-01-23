fecee1a8d6156ef3f39091b01b3ddcef85b7e5f4
Axel Liljencrantz <axel@liljencrantz.se>
2011-01-23 21:27:08 +0100

    Add additional deps

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 CLIQUEROOT=$(pwd)
 
 virtualenv deps
@@ -11,13 +13,14 @@ pip install django-staticfiles
 pip install django-idmapper
 pip install django-registration
 pip install pydot
+pip install jogging
 # Install matplotlib w/o using pip to work around this bug: http://www.mail-archive.com/matplotlib-users@lists.sourceforge.net/msg12827.html
 #pip install matplotlib
 easy_install "http://downloads.sourceforge.net/project/matplotlib/matplotlib/matplotlib-0.98.5/matplotlib-0.98.5.3.tar.gz"
 
 (
- cd "$CLIQUEROOT/deps/lib/python"*"/site-packages/fcdjangoutils"
- git checkout git://github.com/redhog/fcdjangoutils.git
+ cd "$CLIQUEROOT/deps/lib/python"*"/site-packages"
+ git clone git://github.com/redhog/fcdjangoutils.git
 )
 
 wget http://mirror.i2p2.de/i2psource_0.8.1.tar.bz2
