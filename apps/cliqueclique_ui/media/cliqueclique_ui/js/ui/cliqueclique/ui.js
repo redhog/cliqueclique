111189ede8ebb4911ee2b68e700f651a7abe4dbe
Egil Moeller <egil.moller@freecode.no>
2011-02-22 21:11:03 +0100

    Code cleanup

---
+++ apps/cliqueclique_ui/media/cliqueclique_ui/js/ui/cliqueclique/ui.js
@@ -0,0 +1,55 @@
+dojo.provide("cliqueclique.ui");
+
+dojo.require("cliqueclique.document.tree");
+dojo.require("cliqueclique.document.selector");
+dojo.require("cliqueclique.document.editor");
+dojo.require("cliqueclique.document");
+dojo.require("cliqueclique.nodeinfo");
+dojo.require("dijit.layout.BorderContainer");
+dojo.require("dijit.layout.ContentPane");
+dojo.require("dijit.layout.AccordionContainer");
+dojo.require("dijit.layout.TabContainer");
+dojo.require("dojox.layout.TableContainer");
+
+dojo.require("dijit._base.popup");
+dojo.require("dojo.html");
+dojo.require("cliqueclique.general.OptionalTabContainer");
+
+
+dojo.declare("cliqueclique.ui.Ui", [dijit.layout.BorderContainer], {
+  design:'sidebar',
+  style:'border: 0px; height: 100%; width: 100%;',
+  startup: function () {
+    this.inherited(arguments);
+
+    var ui = this;
+
+    ui.leftPane = new dijit.layout.AccordionContainer({region: 'left', splitter: true, minSize: 200});
+    ui.addChild(ui.leftPane);
+
+    ui.tree = new dijit.Tree({showRoot: false, model: cliqueclique.document.tree.DocumentTreeModel(), style: "width: 200px;", title: 'Bookmarks'});
+    ui.leftPane.addChild(ui.tree);
+    ui.tree.connect(ui.tree, 'onClick', function (item, node, evt) { item.getDocumentLink(tree)(); });
+
+    ui.menu = new cliqueclique.document.DocumentMenu({});
+    ui.menu.startup();
+    ui.menu.bindDomNode(ui.tree.domNode);
+
+    ui.docView = cliqueclique.document.DocumentView({region: 'center'});
+    ui.addChild(ui.docView);
+    ui.registerData("documentLink", {label: 'Display', load:function (document) { return ui.docView.attr("document", document); }}, true, "cliqueclique.document.DocumentLink");
+
+    ui.tabCon = cliqueclique.general.OptionalTabContainer({region:'bottom', splitter: true, style:'height: 30%;'});        
+    ui.addChild(ui.tabCon);
+    ui.tabCon.updateVisibility();
+
+    ui.registerData("documentLink",
+		    {label: 'Comment',
+		     load:function (document) {
+		       var docEdit = new cliqueclique.document.editor.DocumentEditor({closable:true, title: 'Kafoo'});
+		       ui.tabCon.addChild(docEdit);
+		     }},
+                     true,
+                     "cliqueclique.document.DocumentLink");
+  }
+});
