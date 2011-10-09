05fd04c8a16b662f3e40d592d745162b2b7bea89
Egil Moeller <egil.moller@freecode.no>
2011-10-09 21:07:22 +0200

    Started using nodeinfo for document view

--- apps/cliqueclique_ui/media/cliqueclique_ui/js/ui/cliqueclique/ui/modules/DocumentView.js
+++ apps/cliqueclique_ui/media/cliqueclique_ui/js/ui/cliqueclique/ui/modules/DocumentView.js
@@ -6,7 +6,7 @@ dojo.require("cliqueclique.document.DocumentLink");
 dojo.require("cliqueclique.document.DocumentMenu");
 dojo.require("dijit.form.CheckBox");
 
-dojo.declare("cliqueclique.document.DocumentBodyView", [dijit._Widget, dijit._Templated, cliqueclique.document._AbstractDocumentView], {
+dojo.declare("cliqueclique.ui.modules.DocumentViewBody", [dijit._Widget, dijit._Templated, cliqueclique.document._AbstractDocumentView], {
   widgetsInTemplate: true,
   templateString: "" +
     "<div class='documentBodyView'>" +
@@ -73,107 +73,22 @@ dojo.declare("cliqueclique.document.DocumentBodyView", [dijit._Widget, dijit._Te
   displayPart: "content"
 });
 
-dojo.declare("cliqueclique.ui.modules.DocumentView", [dijit._Widget, dijit._Templated, cliqueclique.document._AbstractDocumentView], {
+dojo.declare("cliqueclique.ui.modules.DocumentViewAttributes", [dijit._Widget, dijit._Templated, cliqueclique.document._AbstractDocumentView], {
   widgetsInTemplate: true,
   templateString: "" +
-    "<div class='documentView'>" +
-    "  <div class='actionBox'>" +
-    "    <input dojoType='dijit.form.CheckBox' dojoAttachPoint='bookmarkedInput' dojoAttachEvent='onClick:onBookmarkedInputClick'></input><label dojoAttachPoint='bookmarkedLabel'>bookmarked</label>" +
-    "    <input dojoType='dijit.form.CheckBox' dojoAttachPoint='readInput' dojoAttachEvent='onClick:onReadInputClick'></input><label dojoAttachPoint='readLabel'>read</label>" +
-    "    <input dojoType='dijit.form.CheckBox' dojoAttachPoint='subscribedInput' dojoAttachEvent='onClick:onSubscribedInputClick'></input><label dojoAttachPoint='subscribedInput'>subscribed</label><br>" +
-    "    Download: <a dojoAttachPoint='downloadAsMime' target='_new'>as mime</a> or <a dojoAttachPoint='downloadAsJson' target='_new'>as json</a>" +
-    "  </div>" +
-    "  <div>" +
-    "    <h1 dojoAttachPoint='title'></h1>" +
-    "    <div>" +
-    "      Parents:" +
-    "      <span dojoAttachPoint='parentDocuments'></span>" +
-    "    </div>" +
-    "    <div>" +
-    "      Children:" +
-    "      <span dojoAttachPoint='childDocuments'></span>" +
-    "    </div>" +
-    "    <div>" +
-    "      Direct parent links:" +
-    "      <span dojoAttachPoint='directParentDocuments'></span>" +
-    "    </div>" +
-    "    <div>" +
-    "      Direct child links:" +
-    "      <span dojoAttachPoint='directChildDocuments'></span>" +
-    "    </div>" +
-    "    <div dojoAttachPoint='body'></div>" +
-    "  </div>" +
+    "<div class='actionBox'>" +
+    "  <input dojoType='dijit.form.CheckBox' dojoAttachPoint='bookmarkedInput' dojoAttachEvent='onClick:onBookmarkedInputClick'></input><label dojoAttachPoint='bookmarkedLabel'>bookmarked</label>" +
+    "  <input dojoType='dijit.form.CheckBox' dojoAttachPoint='readInput' dojoAttachEvent='onClick:onReadInputClick'></input><label dojoAttachPoint='readLabel'>read</label>" +
+    "  <input dojoType='dijit.form.CheckBox' dojoAttachPoint='subscribedInput' dojoAttachEvent='onClick:onSubscribedInputClick'></input><label dojoAttachPoint='subscribedInput'>subscribed</label><br>" +
+    "  Download: <a dojoAttachPoint='downloadAsMime' target='_new'>as mime</a> or <a dojoAttachPoint='downloadAsJson' target='_new'>as json</a>" +
     "</div>",
-
-  BodyView: cliqueclique.document.DocumentBodyView,
-
-  postCreate: function () {
-    var res = this.inherited(arguments);
-    var menu = new cliqueclique.document.DocumentMenu({});
-    menu.startup();
-    menu.bindDomNode(this.title);
-
-    // Update stuff if we need to (mainly links)
-    dojo.connect(cliqueclique.document.Document, "updated", this, "refresh");
-
-    this.bodyView = new this.BodyView({}, this.body);
-
-    return res;
-  },
-  refresh: function () {
-    var view = this;
-    if (!view.attr("document")) return;
-    cliqueclique.document.Document.find(function (documents) {
-      var document = documents[0];
-      // Check that we haven't changed document since the refresh was asked for...
-      if (document.getDocumentId() == view.attr("document").getDocumentId())
-        view.attr("document", document);
-    }, [], view.attr("document").getDocumentId());
-  },
   _setDocumentAttr: function (document) {
     var documentView = this;
     this.inherited(arguments);
-    this.bodyView.attr("document", document);
-
-
-    function populateLinks(query, field) {
-      cliqueclique.document.Document.find(function (children) {
-	dojo.forEach(children, function (child) {
-	  var link = new cliqueclique.document.DocumentLink({document: child});
-	  dojo.place(link.domNode, documentView[field]);
-	});
-      }, query, documentView.item.getDocumentId());
-    }
-
-    function clearLinks(field) {
-      dojo.query('> *', documentView[field]).forEach(function(domNode, index, arr){
-	dijit.byNode(domNode).destroyRecursive();
-      });
-    }
-
-    clearLinks("childDocuments");
-    clearLinks("parentDocuments");
-    clearLinks("directChildDocuments");
-    clearLinks("directParentDocuments");
-
-    if (!this.item) {
-      dojo.html.set(this.title, "No document selected");
-
-      this.downloadAsMime.href = "";
-      this.downloadAsJson.href = "";
-
-      this.bookmarkedInput.attr("value", false);
-      this.readInput.attr("value", false);
-      this.subscribedInput.attr("value", false);
-
-      return;
-    }
 
     if (!this.item.json_data.read) {
       this.item.setAttribute("read", true);
     }
-
-    dojo.html.set(this.title, this.item.getSubject());
 
     this.downloadAsMime.href = "/find/mime/" + this.item.getDocumentId();
     this.downloadAsJson.href = "/find/json/" + this.item.getDocumentId();
@@ -181,11 +96,6 @@ dojo.declare("cliqueclique.ui.modules.DocumentView", [dijit._Widget, dijit._Temp
     this.bookmarkedInput.attr("value", this.item.json_data.bookmarked);
     this.readInput.attr("value", this.item.json_data.read);
     this.subscribedInput.attr("value", this.item.json_data.local_is_subscribed);
-
-    populateLinks("->", "childDocuments");
-    populateLinks("<-", "parentDocuments");
-    populateLinks(">", "directChildDocuments");
-    populateLinks("<", "directParentDocuments");
   },
   onBookmarkedInputClick: function () {
    this.item.setAttribute("bookmarked");
@@ -198,9 +108,132 @@ dojo.declare("cliqueclique.ui.modules.DocumentView", [dijit._Widget, dijit._Temp
   }
 });
 
+dojo.declare("cliqueclique.ui.modules.DocumentViewLinks", [dijit._Widget, dijit._Templated, cliqueclique.document._AbstractDocumentView], {
+  widgetsInTemplate: true,
+  templateString: "" +
+    "<div class='documentViewLinks'>" +
+    "  <h1 dojoAttachPoint='title'></h1>" +
+    "  <div>" +
+    "    Parents:" +
+    "    <span dojoAttachPoint='parentDocuments'></span>" +
+    "  </div>" +
+    "  <div>" +
+    "    Children:" +
+    "    <span dojoAttachPoint='childDocuments'></span>" +
+    "  </div>" +
+    "  <div>" +
+    "    Direct parent links:" +
+    "    <span dojoAttachPoint='directParentDocuments'></span>" +
+    "  </div>" +
+    "  <div>" +
+    "    Direct child links:" +
+    "    <span dojoAttachPoint='directChildDocuments'></span>" +
+    "  </div>" +
+    "</div>",
+  postCreate: function () {
+    var res = this.inherited(arguments);
+    var menu = new cliqueclique.document.DocumentMenu({});
+    menu.startup();
+    menu.bindDomNode(this.title);
+    return res;
+  },
+  _setDocumentAttr: function (document) {
+    var documentView = this;
+    this.inherited(arguments);
+
+    function populateLinks(query, field) {
+      cliqueclique.document.Document.find(function (children) {
+	dojo.forEach(children, function (child) {
+	  var link = new cliqueclique.document.DocumentLink({document: child});
+	  dojo.place(link.domNode, documentView[field]);
+	});
+      }, query, documentView.item.getDocumentId());
+    }
+
+    dojo.html.set(this.title, this.item.getSubject());
+
+    populateLinks("->", "childDocuments");
+    populateLinks("<-", "parentDocuments");
+    populateLinks(">", "directChildDocuments");
+    populateLinks("<", "directParentDocuments");
+  }
+});
+
+dojo.declare("cliqueclique.ui.modules.DocumentViewEmpty", [dijit._Widget, dijit._Templated, cliqueclique.document._AbstractDocumentView], {
+  widgetsInTemplate: true,
+  templateString: "<div>No document selected</div>",
+});
+
+dojo.declare("cliqueclique.ui.modules.DocumentView", [dijit._Widget, dijit._Templated, cliqueclique.document._AbstractDocumentView], {
+  widgetsInTemplate: true,
+  templateString: "<div class='documentView' dojoAttachPoint='views'></div>",
+  postCreate: function () {
+    var res = this.inherited(arguments);
+    // Update stuff if we need to (mainly links)
+    dojo.connect(cliqueclique.document.Document, "updated", this, "refresh");
+    return res;
+  },
+  refresh: function () {
+    var documentView = this;
+    if (!view.attr("document")) return;
+    cliqueclique.document.Document.find(function (documents) {
+      var document = documents[0];
+      // Check that we haven't changed document since the refresh was asked for...
+      if (document.getDocumentId() == view.attr("document").getDocumentId())
+        documentView.attr("document", document);
+    }, [], documentView.attr("document").getDocumentId());
+  },
+  _setDocumentAttr: function (document) {
+    var documentView = this;
+    this.inherited(arguments);
+
+    dojo.query('> *', documentView.views).forEach(function(domNode, index, arr){
+      dijit.byNode(domNode).destroyRecursive();
+    });
+
+    dojo.forEach(this.getData("documentView", "cliqueclique.ui.modules.DocumentView"), function (item, i) {
+      item.load(documentView, document);
+    });
+  },
+});
+
+
 cliqueclique.ui.modules.DocumentView.register = function (widget) {
-  widget.docView = cliqueclique.ui.modules.DocumentView({region: 'center'});
-  widget.inner.addChild(ui.docView);
+  widget.registerData("documentView",
+                      {load:function (documentView, document) {
+			if (document) return;
+		        var subView = new cliqueclique.ui.modules.DocumentViewEmpty();
+			dojo.place(subView.domNode, documentView.views);
+		      }},
+		      false,
+		      "cliqueclique.ui.modules.DocumentView");
+
+  widget.registerData("documentView",
+                      {load:function (documentView, document) {
+			if (!document) return;
+		        var subView = new cliqueclique.ui.modules.DocumentViewAttributes({document: document});
+			dojo.place(subView.domNode, documentView.views);
+		      }},
+		      false,
+		      "cliqueclique.ui.modules.DocumentView");
+
+  widget.registerData("documentView",
+                      {load:function (documentView, document) {
+			if (!document) return;
+		        var subView = new cliqueclique.ui.modules.DocumentViewLinks({document: document});
+			dojo.place(subView.domNode, documentView.views);
+		      }},
+		      false,
+		      "cliqueclique.ui.modules.DocumentView");
+
+  widget.registerData("documentView",
+                      {load:function (documentView, document) {
+			if (!document) return;
+		        var subView = new cliqueclique.ui.modules.DocumentViewBody({document: document});
+			dojo.place(subView.domNode, documentView.views);
+		      }},
+		      false,
+		      "cliqueclique.ui.modules.DocumentView");
 
   widget.registerData("documentLink",
 		      {label: 'Display',
@@ -209,4 +242,8 @@ cliqueclique.ui.modules.DocumentView.register = function (widget) {
 		      }},
 		      true,
 		      "cliqueclique.document.DocumentLink");
+
+  widget.docView = cliqueclique.ui.modules.DocumentView({region: 'center'});
+  widget.inner.addChild(widget.docView);
+  widget.docView.attr("document", undefined);
 };
